12d827b346df40e1cdd0a09e6088574fd36ec529
stefanolami <stefanolami90@gmail.com>
2022-03-17 17:03:26 +0100

    added search functionality

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+
 /*   search bar   */
 
-
 const searchBar = document.createElement('div');
+
 searchBar.innerHTML = `
    <label for="search" class="student-search">
       <span>Search by name</span>
@@ -16,33 +17,68 @@ searchBar.innerHTML = `
 
 document.querySelector("header").append(searchBar);
 
+
+/*   declarations   */
+
 const searchInput = document.querySelector("#search");
 const searchBtn = document.querySelector("#search-btn");
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector(".link-list");
 
+
+/*    search functionality    */
+
+
+let newData = [];                    // new array to hold search results
+
+const handleNoResults = () => {
+
+   const noResults = document.createElement("li");
+   noResults.innerHTML = "<li><h3 class='no-results'>No results found</h3></li>";
+   
+   while (studentList.firstChild) {                            // deletes every child in the stusent-list section
+      studentList.removeChild(studentList.firstChild);
+   }
+
+   while (linkList.firstChild) {                               // deletes every child in the link-list section
+      linkList.removeChild(linkList.firstChild);
+   }
+
+   studentList.insertAdjacentHTML("beforeend", noResults.innerHTML);       // append the noResults content
+   
+}
 
 const handleSearch =  () => {
+
+   newData = [];                                                   //  reset array on every search
    let searchValue = searchInput.value.toLowerCase();
-   const newData = [];
-   for (i = 0; i < data.length; i++) {
+
+   for (let i = 0; i < data.length; i++) {
       const fullName = `${data[i].name.first} ${data[i].name.last}`.toLowerCase();
       if (fullName.includes(searchValue)) {
-         newData.push(data[i]);
+         newData.push(data[i]);                             // pushes every match found in the newData array
       }
    }
-   showPage(newData, 1);
-   addPagination(newData);
+
+   if (newData.length === 0) {
+      handleNoResults();
+   } else {
+      showPage(newData, 1);                        
+      addPagination(newData);
+   }
 }
 
 /*  showPage function  */
 
-const showPage = (list, page) => {
+
+const showPage = (list, page) => {                          
+
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
 
-   const ul = document.querySelector('.student-list');
-   ul.innerHTML = "";
+   studentList.innerHTML = "";
 
-   for (i = 0; i < list.length; i++) {
+   for (i = 0; i < list.length; i++) {                                     // creates li for every element in the given array
       if (i >= startIndex && i < endIndex) {
          const li = document.createElement("li");
          li.innerHTML = `
@@ -57,7 +93,7 @@ const showPage = (list, page) => {
                </div>
             </li>`;
 
-         ul.insertAdjacentHTML("beforeend", li.innerHTML)
+         studentList.insertAdjacentHTML("beforeend", li.innerHTML)
       }
    }
 }
@@ -66,24 +102,25 @@ const showPage = (list, page) => {
 /*   addPagination function   */
 
 
-const linkList = document.querySelector(".link-list");
+
 
 const addPagination = (list) => {
+
    const neededBtns = Math.ceil(list.length / 9);
    
    linkList.innerHTML = "";
 
-   for (i = 1; i <= neededBtns; i++) {
-      const li = document.createElement("li");
-      li.innerHTML = `
-         <li>
-            <button type="button" id="btn-${i}">${i}</button>
-         </li>`;
-      linkList.insertAdjacentHTML("beforeend", li.innerHTML);
+   if (neededBtns > 1) {
+      for (i = 1; i <= neededBtns; i++) {
+         const li = document.createElement("li");                             // creates the pagination buttons
+         li.innerHTML = `
+            <li>
+               <button type="button" id="btn-${i}">${i}</button>
+            </li>`;
+         linkList.insertAdjacentHTML("beforeend", li.innerHTML);
+      }
+      document.querySelector("#btn-1").classList.add("active");
    }
-
-   document.querySelector("#btn-1").classList.add("active");
-    
 }
 
 /*   eventListeners   */
@@ -93,21 +130,22 @@ linkList.addEventListener('click', e => {
    if (e.target.type === "button") {
       document.querySelector(".active").classList.remove("active");
       e.target.classList.add("active");
-
-      showPage(data, e.target.textContent);
+      if (newData.length === 0) {
+         showPage(data, e.target.textContent);
+      } else {
+         showPage(newData, e.target.textContent);
+      }
    }
-
 })
 
 
-searchInput.addEventListener("keyup", () => {
+searchInput.addEventListener("keyup", () => {               
    handleSearch();
-});
+})
 
 searchBtn.addEventListener("click", () => {
    handleSearch();
-   console.log('ciao')
-});
+})
 
 
 // Call functions
